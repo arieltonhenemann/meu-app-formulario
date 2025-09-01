6ceac4f8266f8d7e8e1c9524f4341e60973ce8da
Devpalyer <arielton.henemann@gmail.com>
2025-09-01 20:04:12 -0300

    Updates

--- src/shared/index.ts
+++
@@ -1,9 +0,0 @@
-// Exportações principais do módulo compartilhado
-export * from './types';
-export * from './types/os';
-export * from './types/pon';
-export * from './types/link';
-export * from './utils';
-export * from './utils/gerarArquivoTxt';
-export * from './services/api';
-export * from './components/useUsuario';
